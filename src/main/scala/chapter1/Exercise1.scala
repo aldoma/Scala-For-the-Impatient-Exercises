e98d871baacc2fafd9f034684055ea49fb06a830
Alberto Dominguez Matamoros <alberto.dominguez@fon.com>
2019-02-27 15:40:07 +0100

    Resolved exercise 1 of chapter 3

---
+++ src/main/scala/chapter1/Exercise1.scala
@@ -0,0 +1,19 @@
+package chapter1
+
+import scala.util.Random
+
+/*
+Write a code snippet that sets a to an array of n random integers between 0 (inclusive) and n (exclusive).
+ */
+object Exercise1 extends App {
+
+  val random = Random
+
+  def generate(n: Int) = {
+    for (i <- 0 to n) yield
+      random.nextInt(n)
+  }
+
+  println(generate(5))
+  println(generate(8))
+}
