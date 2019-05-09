5eb94009f930f557cd4eedb07f299e614e56ab2e
Alberto Dominguez Matamoros <alberto.dominguez@fon.com>
2019-05-09 15:34:00 +0200

    Add exercise 3 of chapter 6

---
+++ src/main/scala/chapter6/Exercise3.scala
@@ -0,0 +1,11 @@
+package chapter6
+
+/**
+  * Define an Origin object that extends [[java.awt.Point]]. Why is this not actually a
+  * good idea? (Have a close look at the methods of the `Point` class.)
+  */
+object Exercise3 {
+
+}
+
+object Origin extends java.awt.Point
